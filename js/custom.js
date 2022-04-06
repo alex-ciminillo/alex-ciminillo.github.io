a1e221f2080659b60b3c3b9fb87886cd1f8512a2
alex-ciminillo <ciminilloa@findlay.edu>
2022-04-05 21:34:08 -0400

    Add email functionality

--- js/custom.js
+++ js/custom.js
@@ -14,6 +14,18 @@
         
     })
 
+    //send form
+    var submitButton = document.getElementById("submit_form");
+    var form = document.getElementById("email_form");
+    form.addEventListener("submit", function (e) {
+        setTimeout(function() {
+            submitButton.value = "Sending...";
+            submitButton.disabled = true;
+        }, 1);
+    });
+
+
+
     // HEADER
     $(".navbar").headroom();
 
